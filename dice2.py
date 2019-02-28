30214a3b556125279f3416025d5004354e028609
Gulshankumar9 <48007930+Gulshankumar9@users.noreply.github.com>
2019-02-28 20:28:19 +0530

    Update dice2.py

--- dice2.py
+++ dice2.py
@@ -1,2 +1,15 @@
+#Name:Jahnavi T
+#Branch:CSE
+#Batch:AL-2
+
+
+#Random Nos Generator 
 import random
-print(random.randint(1,6))
+print("random no generated : ",random.randint(1,6))
+
+
+output:
+random no generated :  5
+random no generated :  3
+random no generated :  4
+random no generated :  2
